af22aa815773ed7a412eb8845cb8a17c141c9162
QiangQiang <546566696@qq.com>
2017-07-08 16:46:43 +0800

    改

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 # -*- coding:UTF-8 -*-
-import urllib
+#import urllib
 import urllib2
+
+def download(url, user_agent:'wswp', num_retries = 2):
+	print "Downloading:", url
+	headers = ('User-agent': user_agent)
+	request = urllib2.Request(url, headers = headers)
+	
+	try:
+		html = urllib2.urlopen(url).read()
+	except urllib2.URLError as e:
+		print "Download error:", e.reason
+		html = None
+	return html
+ 
